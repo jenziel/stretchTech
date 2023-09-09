8daebf77687375d52e8d47b8fd499e601d85b687
Parvin Sattorova <nanajon66@gmail.com>
2023-09-09 17:00:54 -0600

    feat: Add ParkWrapper with error handling and props interface
    - Add ParkWrapperProps interface
    - Implement ParkWrapper function
    - Add conditional operator for error handling
    - Update JSX return

--- src/components/ParkWrapper/ParksWrapper.tsx
+++ src/components/ParkWrapper/ParksWrapper.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import './ParksWrapper.css'
+import './ParksWrapper.css';
 
 interface ParkWrapperProps {
-    parkId: string;
-    parkName: string;
+  park: {
+    id: string;
+    fullName: string;
+    description: string;
+    activities: Array<{ id: string; name: string }>;
+  } | null;
+}
+
+const ParkWrapper: React.FC<ParkWrapperProps> = ({ park }) => {
+  //I'm defining a new React functional component. This component expects to receive props that match the ParkWrapperProps interface above. TypeScript will enforce it.
+  if (!park) {
+    return <div className="error">Park information is not available.</div>;
   }
-  
-  const ParkWrapper: React.FC<ParkWrapperProps> = (props) => {
-    // Component logic here
-    
+
+  if (!park.activities || park.activities.length === 0) {
     return (
-      // JSX goes here
+      <div className="park-wrapper">
+        <h1>{park.fullName}</h1>
+        <p>{park.description}</p>
+        <h2>Activities</h2>
+        <p>No activities available for this park.</p>
+      </div>
     );
   }
-  
+
   return (
-    <div>
-      <h1>This is the ParkWrapper component.</h1>
+    <div className="park-wrapper">
+      <h1>{park.fullName}</h1>
+      <p>{park.description}</p>
+      <h2>Activities</h2>
+      <ul>
+        {park.activities.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
     </div>
   );
-  
-  export default ParkWrapper;
+};
+
+export default ParkWrapper;
