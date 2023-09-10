0a0d422926d5e89e025d259bde75170fe55511c4
Parvin Sattorova <nanajon66@gmail.com>
2023-09-09 18:45:34 -0600

    feat: Add ParkWrapper with error handling and props interface
    - Add ParkDetailsProps interface
    - Implement ParkDetails function
    - Add conditional operator for error handling
    - Update JSX return and add css file

--- src/components/ParkDetails/ParkDetails.tsx
+++ src/components/ParkDetails/ParkDetails.tsx
@@ -1,1 +1,43 @@
-export {}
+import React from 'react';
+import './ParkDetails.css';
+
+interface ParkDetailsProps {
+  park: {
+    id: string;
+    fullName: string;
+    description: string;
+    activities: Array<{ id: string; name: string }>;
+    images: Array<{ url: string; altText: string }>;
+  } | null;
+}
+
+function ParkDetails(props: ParkDetailsProps) {
+  const { park } = props;
+
+  if (!park) {
+    return <div className="error">Park information is not available.</div>;
+  }
+
+  return (
+    <div className="park-details">
+      <h1>{park.fullName}</h1>
+      <p>{park.description}</p>
+      <h2>Activities</h2>
+      <ul>
+        {park.activities.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
+      <h2>Images</h2>
+      <div className="image-gallery">
+        {park.images.map((image, index) => (
+          <div key={index}>
+            <img src={image.url} alt={image.altText} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ParkDetails;
