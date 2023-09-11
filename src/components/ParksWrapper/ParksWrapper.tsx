d8969cd57a11a2f6f3f7b144f448150ea72fa03f
Parvin Sattorova <nanajon66@gmail.com>
2023-09-10 22:16:01 -0600

    refactor: Add ApiCalls.js file.Refactor App, ParkCards and ParksWrapper.tsx files

--- src/components/ParksWrapper/ParksWrapper.tsx
+++ src/components/ParksWrapper/ParksWrapper.tsx
@@ -1,45 +1,67 @@
+// import React from 'react';
+// import './ParksWrapper.css';
+// import '../ParkCards/ParkCards';
+
+// interface ParksWrapperProps {
+//   parks: Array<{
+//     id: string;
+//     fullName: string;
+//     description: string;
+//     activities: Array<{ id: string; name: string }>;
+//   }> | null;
+// }
+
+// function ParksWrapper(props: ParksWrapperProps) {
+//   const { parks } = props;
+
+//   if (!parks || parks.length === 0) {
+//     return <div className="error">No park information is available.</div>;
+//   }
+
+//   return (
+//     <div className="parks-wrapper">
+//       {parks.map((park) => (
+//         <div key={park.id}>
+//           <h1>{park.fullName}</h1>
+//           <p>{park.description}</p>
+//           <h2>Activities</h2>
+//           {park.activities && park.activities.length > 0 ? (
+//             <ul>
+//               {park.activities.map((activity) => (
+//                 <li key={activity.id}>{activity.name}</li>
+//               ))}
+//             </ul>
+//           ) : (
+//             <p>No activities available for this park.</p>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default ParksWrapper;
+
 import React from 'react';
 import './ParksWrapper.css';
+import { ParkCards, ParkProps } from '../ParkCards/ParkCards';
 
-interface ParksWrapperProps {
-  park: {
-    id: string;
-    fullName: string;
-    description: string;
-    activities: Array<{ id: string; name: string }>;
-  } | null;
-}
-
-function ParksWrapper(props: ParksWrapperProps) {
-  const { park } = props;
-
-  if (!park) {
-    return <div className="error">Park information is not available.</div>;
-  }
-
-  if (!park.activities || park.activities.length === 0) {
+function ParksWrapper({ parks }: { parks: ParkProps[] }) {
+  const parkCards = parks.map(park => {
     return (
-      <div className="parks-wrapper">
-        <h1>{park.fullName}</h1>
-        <p>{park.description}</p>
-        <h2>Activities</h2>
-        <p>No activities available for this park.</p>
-      </div>
+      <ParkCards
+        park={park}
+        key={park.id}
+      />
     );
-  }
+  });
 
   return (
-    <div className="parks-wrapper">
-      <h1>{park.fullName}</h1>
-      <p>{park.description}</p>
-      <h2>Activities</h2>
-      <ul>
-        {park.activities.map((activity) => (
-          <li key={activity.id}>{activity.name}</li>
-        ))}
-      </ul>
+    <div className='parks-container'>
+      {parkCards}
     </div>
   );
 }
 
 export default ParksWrapper;
+
