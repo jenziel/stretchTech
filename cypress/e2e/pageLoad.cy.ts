cc71dd633f0f2d2eb0a5f9e64e02d1f8d800b56f
Jen Ziel <jenziel400@gmail.com>
2023-09-12 09:47:51 -0400

    Edit the tsconfig so that typescript doesn't require typechecking from typescript methods.  Set up cy intercept to stub the GET request on pageload.

---
+++ cypress/e2e/pageLoad.cy.ts
@@ -0,0 +1,10 @@
+const parksUrl = "https://developer.nps.gov/api/v1/parks?limit=500&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc"
+describe('when I visit the app, all national parks should be displayed', () => {
+    beforeEach(() => {
+      cy.intercept("GET", parksUrl,
+       {fixture: 'sampleParks.json'}).as("getParks")
+  })
+  it('should display park cards on page load', () => {
+    cy.visit('http://localhost:3000/')
+  })
+}) 
