6b54cc3058f4b2e3c6c6a3034e63fffb405fe356
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-12 17:35:49 -0600

    Filter parks based on designation

--- src/components/ParksWrapper/ParksWrapper.tsx
+++ src/components/ParksWrapper/ParksWrapper.tsx
@@ -4,7 +4,9 @@ import { ParkCards, ParkProps } from "../ParkCards/ParkCards";
 import HeroImage from "../HeroImage/HeroImage";
 
 function ParksWrapper({ parks }: { parks: ParkProps[] }) {
-  const parkCards = parks.map(park => {
+  
+  const natParkCards = parks.filter((park) => park.designation === 'National Park')
+  const parkCards = natParkCards.map(park => {
     return (
       <ParkCards
         park={park}
