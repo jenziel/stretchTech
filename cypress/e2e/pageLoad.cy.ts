1beb5f802b8fe20d5c70bc255b81d57129d29e87
Jen Ziel <jenziel400@gmail.com>
2023-09-13 08:40:51 -0400

    Add tsconfig.json file to the cypress folder.  This will allow typescript to ignore typechecking cypress methods

--- cypress/e2e/pageLoad.cy.ts
+++ cypress/e2e/pageLoad.cy.ts
@@ -1,4 +1,5 @@
 const parksUrl = "https://developer.nps.gov/api/v1/parks?limit=500&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc"
+
 describe('when I visit the app, all national parks should be displayed', () => {
     beforeEach(() => {
       cy.intercept("GET", parksUrl,
@@ -6,5 +7,7 @@ describe('when I visit the app, all national parks should be displayed', () => {
   })
   it('should display park cards on page load', () => {
     cy.visit('http://localhost:3000/')
+    cy.wait("@getParks")
+    
   })
 }) 
