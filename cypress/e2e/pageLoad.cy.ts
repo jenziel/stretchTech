5a065fce54f5a47800404d34b6e2fec262fb5050
Jen Ziel <jenziel400@gmail.com>
2023-09-13 09:31:05 -0400

    Add cypress tests to iterate through each park in the sample data to check for image and title.

--- cypress/e2e/pageLoad.cy.ts
+++ cypress/e2e/pageLoad.cy.ts
@@ -7,7 +7,17 @@ describe('when I visit the app, all national parks should be displayed', () => {
   })
   it('should display park cards on page load', () => {
     cy.visit('http://localhost:3000/')
-    cy.wait("@getParks")
-    
-  })
-}) 
+    cy.wait("@getParks");
+    cy.contains(".LoadingComponent").should("not.exist");
+    cy.get(".parks-container").should("be.visible");
+    cy.fixture("sampleParks.json").then(sampleParks => {
+        sampleParks.data.forEach((park) => {
+          cy.get('.parks-container h3').contains(park.fullName)
+          cy.get('.parks-container img[src="' + park.images[0].url + '"]')
+            .should("be.visible")
+            
+            });
+        });
+      });
+    });
+  
