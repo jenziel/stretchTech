75b5647dea46eee41fb065c137c64ff1f36cc8f4
Jen Ziel <jenziel400@gmail.com>
2023-09-14 15:23:49 -0400

    Add responsive layout to header component and parksWrapper

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <NavLink to="/">
+      <NavLink to="/" style={{color: `inherit`, textDecoration: `inherit`}}>
         <h1>Parks Passport</h1>
       </NavLink>
-      <NavLink to="/favorites">Favorites</NavLink>
+      <NavLink to="/favorites" style={{color: `inherit`, textDecoration: `inherit`}}>Favorites</NavLink>
     </header>
   );
 }
