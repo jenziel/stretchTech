f94226a812801f46e6e6258ba80b308f5b9035ac
Jen Ziel <jenziel400@gmail.com>
2023-09-14 09:00:54 -0400

    Change in-line styling to inherit textDecoration

--- src/components/ParkCards/ParkCards.tsx
+++ src/components/ParkCards/ParkCards.tsx
@@ -15,7 +15,7 @@ function ParkCards({ park }: { park: ParkProps }) {
   }
 
   return (
-    <Link to={`/park/${park.parkCode}`} style={{ textDecoration: 'none' }}> 
+    <Link to={`/park/${park.parkCode}`} style={{ textDecoration: 'inherit' }}> 
       <div className='park-card'>
         <h3>{park.fullName}</h3>
         <div className="image-gallery">
