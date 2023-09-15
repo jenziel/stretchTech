a968d2030e876730187030417fbdc3a165c5115d
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-15 15:23:03 -0600

    Test and pass favorites spec

--- cypress/e2e/favorite_spec.cy.ts
+++ cypress/e2e/favorite_spec.cy.ts
@@ -6,13 +6,42 @@ describe("when I visit the app, I can favorite and unfavorite parks", () => {
       "getParks"
     );
   });
-  it("should add park to favorites when favorite button is clicked", () => {
+
+  const selectArches = () => {
+    const archesTitle = cy
+      .get(".park-card-lower h3")
+      .contains("Arches National Park");
+    archesTitle.parent().children(".favorite-button").click();
+  };
+
+  const addACardToFavorites = () => {
     cy.visit("http://localhost:3000/");
     cy.wait("@getParks");
     cy.get(".parks-container").should("be.visible");
-    cy.get(".park-card").should("exist");
-    cy.get(".park-card-lower").should("be.visible");
-    cy.get(".favorite-button").should("be.visible");
-    cy.get(".favorite-button").last().click();
+    selectArches();
+  };
+
+  it("should add park to favorites when favorite button is clicked", () => {
+    addACardToFavorites();
+    cy.get("header").contains("Favorites").click();
+    cy.get(".park-card-lower h3")
+      .contains("Arches National Park")
+      .should("exist");
+  });
+
+  it("should remove park from favorites when favorite button is de-selected", () => {
+    addACardToFavorites();
+    cy.get("header").contains("Favorites").click();
+    selectArches();
+    cy.get(".park-card-lower h3").should("not.exist");
+  });
+
+  it("should change classNames when favorited", () => {
+    addACardToFavorites();
+    cy.get(".park-card-lower")
+      .contains("Arches National Park")
+      .parent()
+      .children(".favorite-button.favorite")
+      .should("exist");
   });
 });
