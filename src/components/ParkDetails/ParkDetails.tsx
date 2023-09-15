84099be4330b9fda63c4a7d4511f21e0d58f9295
Parvin Sattorova <nanajon66@gmail.com>
2023-09-14 20:18:03 -0600

    feature:Add additional information to ParkDetails

--- src/components/ParkDetails/ParkDetails.tsx
+++ src/components/ParkDetails/ParkDetails.tsx
@@ -54,11 +54,17 @@ function ParkDetails() {
       </div>
       <div className="additional-info">
         <h2>Additional Information</h2>
-        <p><strong>Entrance Hours:</strong> {park.operatingHours.map(hour => hour.name).join(', ')}</p>
+        <p><strong>Entrance Hours:</strong></p>
+        <ul>
+          {Object.entries(park.operatingHours[0].standardHours).map(([day, hours]) => (
+            <li key={day}>{`${day.charAt(0).toUpperCase() + day.slice(1)}: ${hours}`}</li>
+          ))}
+        </ul>
         <p><strong>Location (State):</strong> {park.states}</p>
-        <p><strong>Entrance Fees:</strong> {park.entranceFees.map(fee => fee.title).join(', ')}</p>
+        <p><strong>Entrance Fees:</strong> {park.entranceFees.map(fee => `$${fee.cost}`).join(', ')}</p>
         <p><strong>Address:</strong> {park.addresses.map(address => address.line1).join(', ')}</p>
         <p><strong>Phone Number:</strong> {park.contacts.phoneNumbers.map(phone => phone.phoneNumber).join(', ')}</p>
+        <p><strong>Directions:</strong> <a href={park.directionsUrl} target="_blank" rel="noopener noreferrer">Click here for directions</a></p>
       </div>
     </div>
   );
