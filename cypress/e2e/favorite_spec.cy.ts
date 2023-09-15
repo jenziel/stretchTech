52fdb8fa0d0a415969518d68aaae98a49a506360
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-15 13:22:51 -0600

    Write first test

--- cypress/e2e/favorite_spec.cy.ts
+++ cypress/e2e/favorite_spec.cy.ts
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("when I visit the app, I can favorite and unfavorite parks", () => {
+  const parksUrl =
+    "https://developer.nps.gov/api/v1/parks?limit=500&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc";
+  beforeEach(() => {
+    cy.intercept("GET", parksUrl, { fixture: "sampleParks.json" }).as(
+      "getParks"
+    );
+  });
+  it("should add park to favorites when favorite button is clicked", () => {
+    cy.visit("http://localhost:3000/");
+    cy.wait("@getParks");
+    cy.get(".parks-container").should("be.visible");
+    cy.get(".park-card").should("exist");
+    cy.get(".park-card-lower").should("be.visible");
+    cy.get(".favorite-button").should("be.visible");
+    cy.get(".favorite-button").last().click();
+  });
+});
