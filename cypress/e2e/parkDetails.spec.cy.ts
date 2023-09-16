8b6bd307d34aedee8e0f93aa9e4bb42a9443c18e
Parvin Sattorova <nanajon66@gmail.com>
2023-09-15 20:59:56 -0600

    refactor:Add refactored ErrorComponent file

--- cypress/e2e/parkDetails.spec.cy.ts
+++ cypress/e2e/parkDetails.spec.cy.ts
@@ -1,65 +1,64 @@
 /// <reference types="cypress" />
 
-// describe('Happy Path - Park Details', () => {
+describe('Happy Path - Park Details', () => {
 
-//   const parksUrl = 'https://developer.nps.gov/api/v1/parks?limit=500&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc';
-//   const individualParkUrl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc';
+  const parksUrl = 'https://developer.nps.gov/api/v1/parks?limit=500&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc';
+  const individualParkUrl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc';
 
-//   beforeEach(() => {
-//     cy.intercept('GET', individualParkUrl, { fixture: 'sampleParks.json' }).as('getPark');
-//     cy.visit('http://localhost:3000/');
-//     cy.contains('.LoadingComponent').should('not.exist');
-//     // cy.pause(); 
-//     // cy.wait('@getPark');
-//     cy.get('.park-card', { timeout: 10000 }).first().click();
-//   });
+  beforeEach(() => {
+    cy.intercept('GET', individualParkUrl, { fixture: 'sampleParks.json' }).as('getPark');
+    cy.visit('http://localhost:3000/');
+    cy.contains('.LoadingComponent').should('not.exist');
+    // cy.wait('@getPark');
+    cy.get('.park-card', { timeout: 10000 }).first().click();
+  });
 
 
-//     it('should display a non-empty park full name', () => {
-//       cy.get('.park-details > h1')
-//     });
+    it('should display a non-empty park full name', () => {
+      cy.get('.park-details > h1')
+    });
 
-//     it('should display a non-empty park description', () => {
-//       cy.viewport(1280, 720);
-//       // cy.get('.park-description').should('not.be.empty').and('contain', 'Mock Description');
-//       cy.get('.park-details p').each(($el, index, $list) => {
-//         cy.log($el.text())
-//       })
-//     });
+    it('should display a non-empty park description', () => {
+      cy.viewport(1280, 720);
+      // cy.get('.park-description').should('not.be.empty').and('contain', 'Mock Description');
+      cy.get('.park-details p').each(($el, index, $list) => {
+        cy.log($el.text())
+      })
+    });
 
-//     it('should display a non-empty activities list', () => {
-//       cy.get('.activities ul').should('not.be.empty');
-//     });
+    it('should display a non-empty activities list', () => {
+      cy.get('.activities ul').should('not.be.empty');
+    });
 
-//     it('should display a non-empty state', () => {
-//       cy.get('.additional-info > :nth-child(4)')
-//     });
+    it('should display a non-empty state', () => {
+      cy.get('.additional-info > :nth-child(4)')
+    });
 
-//     it('should display non-empty contacts', () => {
-//       cy.get('.contacts').should('not.be.empty');
-//     });
+    it('should display non-empty contacts', () => {
+      cy.get('.contacts').should('not.be.empty');
+    });
 
-//     it('should display non-empty entrance fees', () => {
-//       cy.get('.entrance-fees').should('not.be.empty');
-//     });
+    it('should display non-empty entrance fees', () => {
+      cy.get('.entrance-fees').should('not.be.empty');
+    });
 
-//     it('should display a non-empty directions URL', () => {
-//       cy.get('.additional-info > :nth-child(7)')
-//     });
+    it('should display a non-empty directions URL', () => {
+      cy.get('.additional-info > :nth-child(7)')
+    });
 
-//     it('should display non-empty operating hours', () => {
-//       cy.get('.operating-hours').should('not.be.empty');
-//     });
+    it('should display non-empty operating hours', () => {
+      cy.get('.operating-hours').should('not.be.empty');
+    });
 
-//     it('should display non-empty addresses', () => {
-//       cy.get('.addresses').should('not.be.empty');
-//     });
+    it('should display non-empty addresses', () => {
+      cy.get('.addresses').should('not.be.empty');
+    });
 
-//     it('should display non-empty images', () => {
-//       cy.get('.images').should('not.be.empty');
-//     });
+    it('should display non-empty images', () => {
+      cy.get('.images').should('not.be.empty');
+    });
 
-//   });
+  });
 
 describe('Sad Path - Park Details', () => {
   const individualParkUrl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=88uiVoPed9zuR3daHPnsrPxaYV0ZWsiqP66VvpSc';
@@ -79,9 +78,5 @@ describe('Sad Path - Park Details', () => {
     cy.get('img')
   });
   
-  // it('should display a placeholder image when the actual image is missing', () => {
-  //   cy.get('.images img').should('have.attr', 'src', '/path/to/placeholder/image');
-  // });
-
 });
 
